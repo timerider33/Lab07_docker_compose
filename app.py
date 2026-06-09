46e4d696af397ce57aa290b5b13c781c70031100
Dmitriy Podtikhov <286739611+timerider33@users.noreply.github.com>
2026-06-09 20:55:14 +0500

    App.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import time
+
+import redis
+from flask import Flask
+# Flask приложение
+app = Flask(__name__)
+# Клиент для подключения к Redis по адресу redis:6379
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    return cache.incr('hits')
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
