c57447135fa5c62d3c855252b7b9161f37788573
Dmitriy Podtikhov <286739611+timerider33@users.noreply.github.com>
2026-06-09 22:50:41 +0500

    flask-redis

--- flask_redis/app.py
+++ flask_redis/app.py
@@ -16,3 +16,20 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    return cache.incr('hits')
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
